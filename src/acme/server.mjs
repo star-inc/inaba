14e7b0250ef66e1e18f942d324788813d116e0f8
Shiranui <supersonic@livemail.tw>
2023-12-24 23:18:50 +0800

    refactor(server.mjs): add log while initializing acme

--- src/acme/server.mjs
+++ src/acme/server.mjs
@@ -38,7 +38,11 @@ export const loadCertificateFiles = () => {
 
   return Promise.all(serverNames.
     filter((serverName) => !isCertificateReady(serverName)).
-    map((serverName) => issueCertificate(serverName))
+    map(async (serverName) => {
+      console.info(`[ACME Initialize] Issuing certificate for \"${serverName}\"`);
+      await issueCertificate(serverName);
+      console.info(`[ACME Initialize] Issued certificate for \"${serverName}\".`);
+    })
   );
 }
 
