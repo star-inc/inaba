541ea981aa56bff7f8c85a70b91d05a9cdc6764c
SuperSonic <supersonic@livemail.tw>
2023-12-25 22:25:57 +0800

    feat(server.mjs): add bottleHeartbeat

--- src/bottle/server.mjs
+++ src/bottle/server.mjs
@@ -11,6 +11,16 @@ export const sessionPool = new Map();
 export const sessionRequests = new Map();
 
 export const bottleServer = new WebSocketServer({ noServer: true });
+export const bottleHeartbeat = setInterval(() => {
+    bottleServer.clients.forEach((ws) => {
+        if (ws.isAlive === false) {
+            return ws.terminate();
+        }
+
+        ws.isAlive = false;
+        ws.ping();
+    });
+}, 30000);
 
 bottleServer.on('connection', function connection(ws) {
     sessionPool.set(ws.sessionId, ws);
@@ -22,4 +32,8 @@ bottleServer.on('connection', function connection(ws) {
     ws.on('message', onMessage);
     ws.on('error', onError);
     ws.on('close', onClose);
-})
+});
+
+bottleServer.on('close', () => {
+    clearInterval(bottleHeartbeat);
+});
